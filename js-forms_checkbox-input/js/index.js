4c38f7c0b226b8f17ac973d3a99cfbffcb469cbe
Moritz Schulenburg <moritzschulenburg@web.de>
2023-03-23 16:26:42 +0100

    finish checkbox

--- js-forms_checkbox-input/js/index.js
+++ js-forms_checkbox-input/js/index.js
@@ -15,6 +15,13 @@ form.addEventListener("submit", (event) => {
   event.preventDefault();
 
   // --v-- write your code here --v--
+  const formElements = event.target.elements;
+  const formData = new FormData(event.target);
+  const data = Object.fromEntries(formData);
+  console.log("data: ", data);
+
+  const tos = formElements.tos.checked;
+  console.log("tos: ", tos);
 
   // --^-- write your code here --^--
 
