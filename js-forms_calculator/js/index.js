2bf525e7d0bbc7ae7ad08c9103a2a3d759639092
Moritz Schulenburg <moritzschulenburg@web.de>
2023-03-24 10:00:17 +0100

    calculator nearly finish

--- js-forms_calculator/js/index.js
+++ js-forms_calculator/js/index.js
@@ -21,8 +21,24 @@ function divide(a, b) {
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
+  const numA = parseInt(form.numberA.bvalue);
+  const numB = parseInt(form.numberB.bvalue);
+  console.log(numA, numB);
 
   let result;
+
+  if (data.operator === "addition") {
+    result = add(numberA, numberB);
+  } else if (data.operator === "subtraction") {
+    result = subtract(numberA, numberB);
+  } else if (data.operator === "multiplication") {
+    result = multiply(numberA, numberB);
+  } else if (data.operator === "division" && numberB !== 0) {
+    result = divide(numberA, numberB);
+  } else if (data.operator === "division" && numberB === 0) {
+    result = "no";
+    close();
+  }
 
   // --v-- write your code here --v--
 
