1671132377c5669e3f60b8ff20f2a3b4046e4ac5
Moritz Schulenburg <moritzschulenburg@web.de>
2023-03-24 11:26:08 +0100

    finish toast message

--- js-createelement_toast-messages/js/index.js
+++ js-createelement_toast-messages/js/index.js
@@ -6,8 +6,16 @@ const clearButton = document.querySelector('[data-js="clear-button"]');
 
 addButton.addEventListener("click", () => {
   // Exercise: Append a new entry to the toast messages container
+  const listElement = document.createElement("li");
+
+  toastContainer.append(listElement);
+
+  listElement.classList.add("toast-container__message");
+  listElement.textContent = "Im a new message";
 });
 
 clearButton.addEventListener("click", () => {
   // Exercise: Clear the stack of toast messages
+  const toastContainer = document.querySelector('[data-js="toast-container"]');
+  toastContainer.innerHTML = "";
 });
