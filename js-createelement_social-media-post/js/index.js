350cf0c6abeb3cc46a1ab37ce54a8034a8c360cf
Moritz Schulenburg <moritzschulenburg@web.de>
2023-03-24 14:03:34 +0100

    first version of challenge- not finish yet

--- js-createelement_social-media-post/js/index.js
+++ js-createelement_social-media-post/js/index.js
@@ -10,3 +10,36 @@ likeButton.addEventListener("click", handleLikeButtonClick);
 
 // Exercise:
 // Use document.createElement() and append another social media post to the body.
+const newPostContent = "Lorem Lorem Lorem Lorem";
+const newUsername = "@loremlorem";
+
+const newPost = document.createElement("article");
+const newParagaph = document.createElement("p");
+const newFooter = document.createElement("footer");
+const newSpan = document.createElement("span");
+const newButton = document.createElement("button");
+
+//styling the the classes
+newPost.classList.add("post");
+newParagaph.classList.add("post__content");
+newFooter.classList.add("post__footer");
+newSpan.classList.add("post__username");
+newButton.classList.add("post__button");
+newButton.setAttribute("type", "button");
+newButton.setAttribute("data-js", "like-button2");
+
+//
+
+newSpan.innerText = newUsername;
+newParagaph.innerText = newPostContent;
+newButton.innerText = "♥ Like";
+
+newFooter.append(newSpan);
+newFooter.append(newButton);
+newPost.append(newParagaph);
+newPost.append(newFooter);
+
+document.body.append(newPost);
+
+const likeButton2 = document.querySelector('[data-js="like-button2"]');
+likeButton2.addEventListener("click", handleLikeButtonClick);
