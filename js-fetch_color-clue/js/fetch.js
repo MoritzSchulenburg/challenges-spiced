a188a7be6b753f8484151d0a7539de64e1284906
Moritz Schulenburg <moritzschulenburg@web.de>
2023-04-05 10:28:46 +0200

    nearly finish challenge

--- js-fetch_color-clue/js/fetch.js
+++ js-fetch_color-clue/js/fetch.js
@@ -3,6 +3,24 @@ import { setColorToGuess, getRandomHexCode } from "./utils.js";
 export async function fetchNewColor() {
   const hexCode = getRandomHexCode();
   const colorApiUrl = `https://www.thecolorapi.com/id?hex=${hexCode}`;
+  // console.log(colorApiUrl);
+
+  try {
+    const response = await fetch(colorApiUrl);
+    if (response.ok) {
+      console.log("bad request");
+    } else {
+      console.log("goood!");
+      const data = await response.json();
+      console.log("data", data.name.value);
+      console.log("data", data.name.closest_name_hex);
+      const name = data.name.value;
+      const closest_hex
+      setColorToGuess(arg1,arg2)
+    }
+  } catch (error) {
+    console.log(error);
+  }
   /**
    * Hint 1:
    * Use the fetch API to get the hex value and the name of the closest
