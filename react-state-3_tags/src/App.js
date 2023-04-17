78c7a336b545a158179a7b52ad2b500a50d42439
Moritz Schulenburg <moritzschulenburg@web.de>
2023-04-17 13:56:12 +0200

    finish tags

--- react-state-3_tags/src/App.js
+++ react-state-3_tags/src/App.js
@@ -6,10 +6,20 @@ import List from "./components/List/index.js";
 export default function App() {
   const [tags, setTags] = useState(["JavaScript", "React", "CSS", "HTML"]);
 
+  function handleAddTag(newTag) {
+    console.log(newTag);
+    setTags([...tags, newTag]);
+  }
+
+  function handleDeleteTag(tagToDelete) {
+    console.log(tagToDelete);
+    setTags(tags.filter((tag) => tag !== tagToDelete));
+  }
+
   return (
     <main className="app">
-      <Form />
-      <List tags={tags} />
+      <Form onAddTag={handleAddTag} />
+      <List onDeleteTag={handleDeleteTag} tags={tags} />
     </main>
   );
 }
