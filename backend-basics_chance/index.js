3609b8336e29312fc741d0ea8bab1eb89d260c0d
Moritz Schulenburg <moritzschulenburg@web.de>
2023-05-04 11:25:48 +0200

    finish response

--- backend-basics_chance/index.js
+++ backend-basics_chance/index.js
@@ -1,3 +1,7 @@
-// import { server } from "./server.js";
+import { server } from "./server.js";
 
 // server.listen(…
+const port = 8888;
+server.listen(port, () => {
+  console.log(`Server running at http://127.0.0.1:${port}/`);
+});
